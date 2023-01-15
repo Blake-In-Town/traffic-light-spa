00e1933c24989643af3446975d16d8e588eb3f9b
Blake-In-Town <boobies4size@gmail.com>
2023-01-15 21:34:20 +0300

    color count dynamic

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+
+export interface Light {
+  color: string,
+  time?: number,
+}
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.sass']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'traffic-light';
+  public lightCount: number = 2;
+  public lights: Light[] = [];
+
+  public setLigthsCount = (lightCount: number): void => {
+    this.lights = new Array<Light>(lightCount);
+    console.log('this.lights: ', this.lights);
+  }
+
+  ngOnInit(): void {
+    this.setLigthsCount(this.lightCount)
+  }
 }
+
